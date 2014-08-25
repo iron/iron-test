5663be1fac4d54d0720dbabda3aac89ba814a0b7
Jonathan Reem <jonathan.reem@gmail.com>
2014-08-25 16:20:41 -0700

    (fix) Update for latest Iron.

--- src/lib.rs
+++ src/lib.rs
@@ -12,40 +12,39 @@ extern crate url;
 pub mod mock {
     /// Contains constructors for mocking Iron Requests.
     pub mod request {
-        use url;
-        use iron::{Request, Alloy};
+        use iron::{Request, TypeMap, Url};
         use http::method;
         use http::headers::request::HeaderCollection;
 
         /// Create a new request at `/` on the specified host with the
         /// specified method.
         pub fn new<S: Str>(method: method::Method, host: S) -> Request {
-            let url = url::Url::parse("http://".to_string().append(host.as_slice()).as_slice()).unwrap();
+            let url = Url::parse("http://".to_string().append(host.as_slice()).as_slice()).unwrap();
             at(method, url)
         }
 
         /// Create a new request at the specific Url with the specified method.
-        pub fn at(method: method::Method, path: url::Url) -> Request {
+        pub fn at(method: method::Method, path: Url) -> Request {
             at_with(method, path, "")
         }
 
         /// Create a new request at the specified Url with the specified method
         /// and the specified content as the body of the request.
-        pub fn at_with<S: Str>(method: method::Method, path: url::Url, body: S) -> Request {
+        pub fn at_with<S: Str>(method: method::Method, path: Url, body: S) -> Request {
             Request {
                 url: path,
                 body: body.as_slice().to_string(),
                 method: method,
                 remote_addr: None,
                 headers: box HeaderCollection::new(),
-                alloy: Alloy::new()
+                extensions: TypeMap::new()
             }
         }
     }
 
     /// Contains constructors for mocking Iron Responses.
     pub mod response {
-        use iron::Response;
+        use iron::{Response, TypeMap};
         use http::status;
         use http::headers::response::HeaderCollection;
 
@@ -55,18 +54,20 @@ pub mod mock {
         /// Create a new, blank, response.
         pub fn new() -> Response {
             Response {
-                body: box MemReader::new(vec![]) as Box<Reader>,
+                body: None,
                 headers: box HeaderCollection::new(),
-                status: None
+                status: None,
+                extensions: TypeMap::new()
             }
         }
 
         /// Create a new response with the specified body and status.
         pub fn with<B: BytesContainer>(status: status::Status, body: B) -> Response {
             Response {
-                body: box MemReader::new(body.container_as_bytes().to_vec()) as Box<Reader>,
+                body: Some(box MemReader::new(body.container_as_bytes().to_vec()) as Box<Reader>),
                 headers: box HeaderCollection::new(),
-                status: Some(status)
+                status: Some(status),
+                extensions: TypeMap::new()
             }
         }
     }
@@ -77,24 +78,24 @@ mod test {
     mod request {
         use super::super::mock::request;
         use http::method;
-        use url::Url;
+        use iron::Url;
 
         #[test] fn test_new() {
             let req = request::new(method::Get, "localhost:3000");
             assert_eq!(req.method, method::Get);
-            assert_eq!(req.url.serialize().as_slice(), "http://localhost:3000/");
+            assert_eq!(format!("{}", req.url).as_slice(), "http://localhost:3000/");
         }
 
         #[test] fn test_at() {
             let req = request::at(method::Post, Url::parse("http://www.google.com/").unwrap());
             assert_eq!(req.method, method::Post);
-            assert_eq!(req.url.serialize().as_slice(), "http://www.google.com/");
+            assert_eq!(format!("{}", req.url).as_slice(), "http://www.google.com:80/");
         }
 
         #[test] fn test_at_with() {
             let req = request::at_with(method::Put, Url::parse("http://www.google.com/").unwrap(), "Hello Google!");
             assert_eq!(req.method, method::Put);
-            assert_eq!(req.url.serialize().as_slice(), "http://www.google.com/");
+            assert_eq!(format!("{}", req.url).as_slice(), "http://www.google.com:80/");
             assert_eq!(req.body.as_slice(), "Hello Google!");
         }
     }
@@ -104,15 +105,15 @@ mod test {
         use http::status;
 
         #[test] fn test_new() {
-            let mut res = response::new();
+            let res = response::new();
             assert_eq!(res.status, None);
-            assert_eq!(res.body.read_to_string().unwrap().as_slice(), "");
+            assert!(res.body.is_none());
         }
 
         #[test] fn test_with() {
-            let mut res = response::with(status::Ok, "Hello World!");
+            let res = response::with(status::Ok, "Hello World!");
             assert_eq!(res.status, Some(status::Ok));
-            assert_eq!(res.body.read_to_string().unwrap().as_slice(), "Hello World!");
+            assert_eq!(res.body.unwrap().read_to_string().unwrap().as_slice(), "Hello World!");
         }
     }
 }
